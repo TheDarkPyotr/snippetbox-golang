f4acc6bc6bb301e78042d17600a155f772f21050
luca <lucapinta@live.it>
2022-08-14 15:22:52 +0200

    Added helpers to human status response, isolate routes from main, improved error tracing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### SnippetBox Golang
 - :white_check_mark: Basic routing
-- :building_construction: Configuration management and error handling
+- :white_check_mark: Configuration management and error handling
 - :building_construction: Database connection management
 - :building_construction:  Dynamic template rendering
 - :building_construction:  Middleware
@@ -11,3 +11,6 @@
 - :building_construction:  User authentication and authorization
 - :building_construction:  Golang context
 - :building_construction:  Unit test, E2E test, integration test
+
+#### To do
+- Configuration management from `config.json`
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,62 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+type Config struct {
+	Addr      string
+	StaticDir string
+}
+
 func main() {
+
+	cfg := new(Config)
+	//If not passed, listen on standard port 4000
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP Network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	// Create a file server which serves files out of the "./ui/static" directo
 	// Note that the path given to the http.Dir function is relative to the pro
 	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 
 	// Use the mux.Handle() function to register the file server as the handler
 	// all URL paths that start with "/static/". For matching paths, we strip the
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	infoLog.Printf("Starting server on %s\n", cfg.Addr)
+	service := &http.Server{
+		Addr:     *&cfg.Addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	err := service.ListenAndServe()
+	if err != nil {
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+		errorLog.Fatal(err)
+
+	}
+
 }
